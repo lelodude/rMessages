4f7086836d1fd33c196cbeeea81cbf0c79a9801d
Lelo <robotelectronic@hotmail.com>
2012-08-21 00:12:29 -0300

    Update src/org/royaldev/royalmessages/RoyalMessages.java

--- src/org/royaldev/royalmessages/RoyalMessages.java
+++ src/org/royaldev/royalmessages/RoyalMessages.java
@@ -1,22 +1,22 @@
 package org.royaldev.royalmessages;
 
 /*
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
 
- This plugin was written by jkcclemens <jkc.clemens@gmail.com>.
- If forked and not credited, alert him.
- */
+This plugin was originally written by jkcclemens and is now taken over by Lelo.
+If forked and not credited, alert me at bukkit.org!
+*/
 
 import com.onarandombox.MultiverseCore.MultiverseCore;
 import com.onarandombox.MultiverseCore.api.MultiverseWorld;
@@ -27,7 +27,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
 
-public class RoyalMessages extends JavaPlugin {
+public class rMessages extends JavaPlugin {
 
     public String version;
 
